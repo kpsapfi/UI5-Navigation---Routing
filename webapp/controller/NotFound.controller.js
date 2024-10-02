50f00c869153b67973a4187585f6823967a525a2
kalen <kalen.peredo@sapfioneer.com>
2024-10-02 10:07:10 -0600

    Enable targets to be displayed without changing the hash (URL)
    Display target manually when there is an invalid URL by calling the targets through the router and displaying. Variable added and passed to router when navigating from home to preserve data that signifies where user just navigated to; this is important information to keep to prevent the user from getting stuck on the notfound page and to keep the back button functional. NotFound.controller.js() logic changed to reflect these changes

--- webapp/controller/NotFound.controller.js
+++ webapp/controller/NotFound.controller.js
@@ -5,6 +5,28 @@ sap.ui.define([
     return BaseController.extend("sap.ui.demo.nav.controller.NotFound", {
         onInit: function() {
 
+            let oRouter;
+            let oTarget;
+
+            oRouter = this.getRouter();
+            oTarget = oRouter.getTarget("notFound");
+            oTarget.attachDisplay(function (oEvent) {
+                this._oData = oEvent.getParameter("data"); // store the data
+            })
+
+        },
+
+        // Override the parent's onNavBack (inherited from BaseController)
+        onNavBack: function () {
+            // In some cases, we could display a certain target when the back button is pressed
+            if (this._oData && this._oData.fromTarget) {
+                this.getRouter().getTargets().display(this._oData.fromTarget);
+                delete this._oData.fromTarget;
+                return;
+            }
+
+            // Call the parent's onNavBack
+            BaseController.prototype.onNavBack.apply(this, arguments);
         }
     });
 });
