61a2117d6bcb180704c244ba489a475fb591b8bf
kalen <kalen.peredo@sapfioneer.com>
2024-10-02 12:07:57 -0600

    Add employee details page By adding route patterns with employee ID parameters from the model, an employee's details can be dynamically loaded based on which employee is clicked. Routing updated in manifest.json. New view and controller files created.

--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -4,5 +4,16 @@ sap.ui.define([
     "use strict";
     
     return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
+        onListItemPressed : function(oEvent) {
+            let oItem;
+            let oCtx;
+
+            oItem = oEvent.getSource();
+            oCtx = oItem.getBindingContext();
+
+            this.getRouter().navTo("employee", {
+                employeeId : oCtx.getProperty("EmployeeID")
+            });
+        }
     });
 });
