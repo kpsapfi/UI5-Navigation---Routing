68d678c4af14f1a6966978895c6d5f7aea350879
kalen <kalen.peredo@sapfioneer.com>
2024-10-03 10:30:47 -0600

    Enable bookmarkable tabs
    Implement optional query parameters to be passed when the user selects a different tab on the resume page. The URL is now unique for each tab rather than being the same when selecting different tabs on the page.

--- webapp/controller/employee/Resume.controller.js
+++ webapp/controller/employee/Resume.controller.js
@@ -1,16 +1,22 @@
 sap.ui.define([
-    "sap/ui/demo/nav/controller/BaseController"
-], function (BaseController) {
+    "sap/ui/demo/nav/controller/BaseController",
+    "sap/ui/model/json/JSONModel"
+], function (BaseController, JSONModel) {
     "use strict";
+
+    let _aValidTabKeys= ["Info", "Projects", "Hobbies", "Notes"];
 
     return BaseController.extend("sap.ui.demo.nav.controller.employee.Resume", {
         onInit: function () {
             let oRouter = this.getRouter();
+            this.getView().setModel(new JSONModel(), "view");
+
             oRouter.getRoute("employeeResume").attachMatched(this._onRouteMatched, this);
         },
         _onRouteMatched: function (oEvent) {
             let oArgs;
             let oView;
+            let oQuery;
 
             oArgs = oEvent.getParameter("arguments");
             oView = this.getView();
@@ -26,12 +32,36 @@ sap.ui.define([
                     }
                 }
             });
+
+            oQuery = oArgs["?query"];
+
+            if (oQuery && _aValidTabKeys.indexOf(oQuery.tab) > -1) {
+                oView.getModel("view").setProperty("/selectedTabKey", oQuery.tab);
+            } else {
+                // The default query param should be visible at all time
+                    this.getRouter().navTo("employeeResume", {
+                        employeeId: oArgs.employeeId,
+                        "?query": {
+                            tab: _aValidTabKeys[0]
+                        }
+                    }, true /*no history */);
+            }
         },
         _onBindingChange: function (oEvent) {
             // No data for the binding
             if (!this.getView().getBindingContext()) {
                 this.getRouter().getTargets().display("notFound");
             };
+        },
+
+        onTabSelect: function (oEvent) {
+            let oCtx = this.getView().getBindingContext();
+            this.getRouter().navTo("employeeResume", {
+                employeeId: oCtx.getProperty("EmployeeID"),
+                "?query": {
+                    tab: oEvent.getParameter("selectedKey")
+                }
+            }, true /*without history*/ )
         }
     })
 })
