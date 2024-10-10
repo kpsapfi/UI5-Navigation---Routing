cce559d74e858c04b74b5b68f112f6ca1f653911
kalen <kalen.peredo@sapfioneer.com>
2024-10-10 14:53:15 -0600

    Enable dialogs to be bookmarkable Modify sort button event handler to open the dialog through the router instead of directly

--- webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
+++ webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
@@ -46,10 +46,18 @@ sap.ui.define([
 
             // Sorting via URL hash
             this._applySorter(oQueryParameter.sortField, oQueryParameter.sortDescending);
-        },
+        
+			// show dialog via URL hash
+			if (oQueryParameter.showDialog) {
+				this._oVSD.open();
+			}
+			
+		},
 
-		onSortButtonPressed : function () {
-			this._oVSD.open();
+		onSortButtonPressed : function (oEvent) {
+			let oRouter = this.getRouter();
+			this._oRouterArgs["?query"].showDialog = 1;
+			oRouter.navTo("employeeOverview", this._oRouterArgs);
 		},
 
 		onSearchEmployeesTable : function (oEvent) {
@@ -67,7 +75,12 @@ sap.ui.define([
 					var oSortItem = oEvent.getParameter("sortItem");
 					this._oRouterArgs["?query"].sortField = oSortItem.getKey();
                     this._oRouterArgs["?query"].sortDescending = oEvent.getParameter("sortDescending");
-                    oRouter.navTo("employeeOverview", this._oRouterArgs, true /*without history */);
+                    delete this._oRouterArgs["?query"].showDialog;
+					oRouter.navTo("employeeOverview", this._oRouterArgs, true /*without history */);
+				}.bind(this),
+				cancel: function (oEvent){
+					delete this._oRouterArgs["?query"].showDialog;
+					oRouter.navTo("employeeOverview", this._oRouterArgs, true /*without history*/);
 				}.bind(this)
 			});
 
